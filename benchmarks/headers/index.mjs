e979b7ed3ad075157fe84fbc27c51aea62787b59
Ethan Arrowood <ethan@arrowood.dev>
2021-06-13 15:15:11 -0600

    expand suite

--- benchmarks/headers/index.mjs
+++ benchmarks/headers/index.mjs
@@ -21,6 +21,7 @@ function printBenchmarkSuite (operation, benchmarks) {
   })
   console.table(output)
 }
+
 function printResults (results) {
   console.log('Benchmark results for Headers class')
   console.log(`Tested modules: ${results.modules.join(', ')}\n`)
@@ -33,11 +34,12 @@ function printResults (results) {
 /** Shuffle list for accurate sorting benchmarks */
 commonHeaderKeys.sort(() => Math.random() - 0.5)
 
-function spawnWorker (operation, module, commonHeaderKeys) {
+function spawnSuite (workerData) {
   return new Promise((resolve, reject) => {
-    const worker = new Worker(new URL(`./${operation}.mjs`, import.meta.url), {
-      workerData: { module, commonHeaderKeys }
-    })
+    const worker = new Worker(
+      new URL(`./suite.mjs`, import.meta.url),
+      { workerData }
+    )
     worker.on('message', resolve)
     worker.on('error', reject)
     worker.on('exit', code => {
@@ -48,12 +50,157 @@ function spawnWorker (operation, module, commonHeaderKeys) {
   })
 }
 
-Promise.all([
-  spawnWorker('append', 'undici-fetch', commonHeaderKeys),
-  spawnWorker('append', 'node-fetch', commonHeaderKeys),
-  spawnWorker('iterate', 'undici-fetch', commonHeaderKeys),
-  spawnWorker('iterate', 'node-fetch', commonHeaderKeys)
-]).then(values => {
+const appendSuite = (module) => spawnSuite({
+  module,
+  operation: 'append',
+  testScript: {
+    path: './scripts/append.mjs',
+    args: [ commonHeaderKeys ]
+  },
+  afterScript: {
+    path: './scripts/assertOrder.mjs',
+    args: [ module, commonHeaderKeys ]
+  }
+})
+
+const setSuite = (module) => spawnSuite({
+  module,
+  operation: 'set',
+  testScript: {
+    path: './scripts/set.mjs',
+    args: [ commonHeaderKeys ]
+  },
+  afterScript: {
+    path: './scripts/assertHas.mjs',
+    args: [ module, commonHeaderKeys ]
+  }
+})
+
+const hasSuite = (module) => spawnSuite({
+  module,
+  operation: 'has',
+  beforeScript: {
+    path: './scripts/append.mjs',
+    args: [ commonHeaderKeys ],
+  },
+  testScript: {
+    path: './scripts/has.mjs',
+    args: [ commonHeaderKeys ]
+  },
+  afterScript: {
+    path: './scripts/assertOrder.mjs',
+    args: [ module, commonHeaderKeys ]
+  }
+})
+
+const getSuite = (module) => spawnSuite({
+  module,
+  operation: 'get',
+  beforeScript: {
+    path: './scripts/append.mjs',
+    args: [ commonHeaderKeys ],
+  },
+  testScript: {
+    path: './scripts/get.mjs',
+    args: [ commonHeaderKeys ]
+  },
+  afterScript: {
+    path: './scripts/assertOrder.mjs',
+    args: [ module, commonHeaderKeys ]
+  }
+})
+
+const deleteSuite = (module) => spawnSuite({
+  module,
+  operation: 'delete',
+  beforeScript: {
+    path: './scripts/append.mjs',
+    args: [ commonHeaderKeys ],
+  },
+  testScript: {
+    path: './scripts/delete.mjs',
+    args: [ commonHeaderKeys ]
+  }
+})
+
+const iterateSuite = (module) => spawnSuite({
+  module,
+  operation: 'iterate',
+  beforeScript: {
+    path: './scripts/append.mjs',
+    args: [ commonHeaderKeys ]
+  },
+  testScript: {
+    path: './scripts/iterate.mjs',
+  }
+})
+
+const forEachSuite = (module) => spawnSuite({
+  module,
+  operation: 'forEach',
+  beforeScript: {
+    path: './scripts/append.mjs',
+    args: [ commonHeaderKeys ]
+  },
+  testScript: {
+    path: './scripts/forEach.mjs',
+  }
+})
+
+const entriesSuite = (module) => spawnSuite({
+  module,
+  operation: 'entries',
+  beforeScript: {
+    path: './scripts/append.mjs',
+    args: [ commonHeaderKeys ]
+  },
+  testScript: {
+    path: './scripts/entries.mjs',
+  }
+})
+
+const keysSuite = (module) => spawnSuite({
+  module,
+  operation: 'keys',
+  beforeScript: {
+    path: './scripts/append.mjs',
+    args: [ commonHeaderKeys ]
+  },
+  testScript: {
+    path: './scripts/keys.mjs',
+  }
+})
+
+const valuesSuite = (module) => spawnSuite({
+  module,
+  operation: 'values',
+  beforeScript: {
+    path: './scripts/append.mjs',
+    args: [ commonHeaderKeys ]
+  },
+  testScript: {
+    path: './scripts/values.mjs',
+  }
+})
+
+const modules = [ 'undici-fetch', 'node-fetch' ]
+const suites = [
+  appendSuite,
+  deleteSuite,
+  entriesSuite,
+  forEachSuite,
+  getSuite,
+  hasSuite,
+  iterateSuite,
+  keysSuite,
+  setSuite,
+  valuesSuite,
+]
+const runs = modules.map(module => suites.map(suite => suite(module))).flat()
+
+try {
+  const values = await Promise.all(runs)
+
   const modules = new Set()
   const benchmarks = {}
   for (let i = 0; i < values.length; i++) {
@@ -67,7 +214,7 @@ Promise.all([
     benchmarks
   }
   printResults(results)
-}).catch(error => {
+} catch (error) {
   console.error(error)
   process.exit(1)
-})
+}
